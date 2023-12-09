d24466127b8c9b0fdc8594e88fb46f9406def405
angie2512 <a20191792@pucp.edu.pe>
2023-12-09 16:52:03 -0500

    cambio

--- app/services/funciones.py
+++ app/services/funciones.py
@@ -1,6 +1,9 @@
 from openstack_sdk import password_authentication_with_scoped_authorization
 from openstack_sdk import token_authentication_with_scoped_authorization
 from openstack_sdk import create_network
+from openstack_sdk import create_subnet
+from openstack_sdk import create_port
+from openstack_sdk import create_instance
 import json
 
 def obtenerTokenAdmin(gateway_ip, admin_password, admin_username, admin_domain_name, domain_id, admin_project_name):
@@ -35,7 +38,7 @@ def obtenerTokenAdmin(gateway_ip, admin_password, admin_username, admin_domain_n
         return None
     
 def crearRed(gateway_ip, token_for_project, network_name):
-    neutron_endpoint = f'http://{gateway_ip}:9696/v2.0'  # Se ha corregido el endpoint
+    neutron_endpoint = f'http://{gateway_ip}:9696/v2.0' 
     resp3 = create_network(neutron_endpoint, token_for_project, network_name)
     if resp3.status_code == 201:
         print('NETWORK CREATED SUCCESSFULLY')
@@ -45,30 +48,81 @@ def crearRed(gateway_ip, token_for_project, network_name):
         print('FAILED NETWORK CREATION')
         return None
     
+def crearSubred(gateway_ip, token_for_project, network_id, subnet_name, ip_version, cidr):
+    neutron_endpoint = f'http://{gateway_ip}:9696/v2.0' 
+    resp = create_subnet(neutron_endpoint, token_for_project, network_id, subnet_name, ip_version, cidr)
+    if resp.status_code == 201:
+        print('SUBNET CREATED SUCCESSFULLY')
+        subnet_created = resp.json()
+        print(json.dumps(subnet_created))
+    else:
+        print('FAILED SUBNET CREATION')
+        return None
+    
+def crearPuerto(gateway_ip, token_for_project, port_name, network_id, project_id):
+    neutron_endpoint = f'http://{gateway_ip}:9696/v2.0' 
+    resp = create_port(neutron_endpoint, token_for_project, port_name, network_id, project_id)
+    if resp.status_code == 201:
+        print('PORT CREATED SUCCESSFULLY')
+        port_created = resp.json()
+        print(json.dumps(port_created))
+    else:
+        print('FAILED PORT CREATION')
+        return None
+    
+def crearInstancia(gateway_ip, token_for_project, instance_name, flavor_id, image_id, networks):
+    nova_endpoint = f'http://{gateway_ip}:8774/v2.1' 
+    resp = create_instance(nova_endpoint, token_for_project, instance_name, flavor_id, image_id, networks)
+    print(resp.status_code)
+    if resp.status_code == 202:
+        print('INSTANCE CREATED SUCCESSFULLY')
+        instance_created = resp.json()
+        print(json.dumps(instance_created))
+    else:
+        print('FAILED INSTANCE CREATION')
+        return None
+    
 
 
 def main():
-    # Input para los datos necesarios
+    #Datos que no se cambian
     gateway_ip = '10.20.10.114'
     admin_password = 'f2d47da4670dc4fb30e1e6e1eed8bb7e'
     admin_username = 'admin'
     admin_domain_name = 'Default'
     domain_id = 'default'
     admin_project_name = 'admin'
+
+    #Datos que deben cambiarse de acuerdo a lo que ingresa el usuario 
     network_name = 'Enlace 1'
+    subnet_name = 'Subred del Enlace 1'
+    ip_version = '4' 
+    cidr = '10.0.39.96/28'
+    network_id = '4af63c6b-a719-401d-b8ee-429f320ba1b0'
+    port_name = 'Enlace 1'
+    project_id = 'c0ee6cd0d5f94c808eb8b5a8813963aa'
+    instance_1_name = 'instance 1'
+    instance_1_flavor_id = '5c199260-3938-4b8a-94e8-9282e915b508'
+    instance_1_image_id = '48d109e3-e4ab-422b-99fa-04a0b70cea7e'
+    instance_1_networks = [{"port": "dfe4c979-297c-4f74-a069-d72201a0db04"}]
+
+    instance_2_name = 'instance 2'
+    instance_2_flavor_id = '5c199260-3938-4b8a-94e8-9282e915b508'
+    instance_2_image_id = '48d109e3-e4ab-422b-99fa-04a0b70cea7e'
+    instance_2_networks = [{"port": "dfe4c979-297c-4f74-a069-d72201a0db04"}]
+
 
     # Obtener el token del administrador
-    project_token = obtenerTokenAdmin(
-        gateway_ip,
-        admin_password,
-        admin_username,
-        admin_domain_name,
-        domain_id,
-        admin_project_name
-    ) 
+    project_token = obtenerTokenAdmin(gateway_ip,admin_password,admin_username,admin_domain_name,domain_id,admin_project_name) 
     if project_token:
         # Crear la red con el token del proyecto
         crearRed(gateway_ip, project_token, network_name)
+        crearSubred(gateway_ip, project_token, network_id, subnet_name, ip_version, cidr)
+        crearPuerto(gateway_ip, project_token, port_name, network_id, project_id)
+        crearInstancia(gateway_ip, project_token, instance_1_name, instance_1_flavor_id, instance_1_image_id, instance_1_networks)
+        crearInstancia(gateway_ip, project_token, instance_2_name, instance_2_flavor_id, instance_2_image_id, instance_2_networks)
+
+
         # Puedes realizar operaciones adicionales aquí utilizando el token de administrador
         print('Operaciones adicionales realizadas exitosamente.')
     else:
