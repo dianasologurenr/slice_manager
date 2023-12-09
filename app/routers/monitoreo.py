9ea1cc1ea3205c23ce60d1be87c198d4e46dcf1f
Miguel <a20190050@pucp.edu.pe>
2023-12-09 17:04:03 -0500

    monitoreo expuesto en la api

--- app/routers/monitoreo.py
+++ app/routers/monitoreo.py
@@ -3,12 +3,21 @@ from fastapi import APIRouter, Depends, HTTPException
 from crud import server as crud_server
 import schemas.schema as schema
 from typing import List
+from pydantic import BaseModel
+from services import monitoreo 
 
 router = APIRouter(
     prefix="/monitoreo",
     tags=["monitoreo"],
     responses={404: {"description": "Not found"}},
 )
+
+class VMInfo(BaseModel):
+    worker: str
+    memory_avail: str
+    memory_total: str
+    memory_usage: str
+    cpu_info: str
 
 @router.get("/servers",response_model=List[schema.Server])
 async def read_server(skip: int = 0, limit: int = 100, db=Depends(get_db)):
@@ -21,6 +30,48 @@ async def read_server(id: int, db=Depends(get_db)):
     if db_server is None:
         raise HTTPException(status_code=404, detail="Server not found")
     return db_server
+
+
+@router.get("/vms_info", response_model=List[VMInfo])
+def get_vm_info():
+    # Configuración de las máquinas virtuales
+    vms = [
+        {"ip": "10.0.0.30", "username": "ubuntu", "password": "ubuntu"},
+        {"ip": "10.0.0.40", "username": "ubuntu", "password": "ubuntu"},
+        {"ip": "10.0.0.50", "username": "ubuntu", "password": "ubuntu"},
+    ]
+
+    # Lista para almacenar los resultados
+    results = []
+
+    # Conexión y obtención de datos de cada máquina virtual
+    for vm in vms:
+        try:
+            ssh_client = monitoreo.get_ssh_connection(vm["ip"], vm["username"], vm["password"])
+
+            memory_avail = monitoreo.get_memory_info(ssh_client)
+            memory_total = monitoreo.get_memory_total(ssh_client)
+            memory_usage = monitoreo.get_memory_usage(ssh_client)
+            cpu_info = monitoreo.get_cpu_info(ssh_client)
+
+            # Almacenar resultados en la lista
+            result = VMInfo(
+                worker=vm["ip"],
+                memory_avail=memory_avail,
+                memory_total=memory_total,
+                memory_usage=memory_usage,
+                cpu_info=cpu_info
+            )
+
+            results.append(result)
+
+        except Exception as e:
+            print(f"Error connecting to {vm['ip']}: {str(e)}")
+
+        finally:
+            ssh_client.close()
+
+    return results
 
 # @router.post("/", response_model=schema.User)
 # async def create_user(user: schema.UserCreate, db=Depends(get_db)):
